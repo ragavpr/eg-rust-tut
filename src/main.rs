3c2218618c40713b15e662365f72e7cd0ec6f5e3
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-06 01:20:02 +0530

    2.4 |P| Compare two numbers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -14,5 +15,11 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
