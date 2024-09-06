396800dc4747de8e916f0b6573639b29634c29bd
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-06 21:41:17 +0530

    12.9 | Config building returns Error instead of panicking

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -21,13 +21,13 @@ struct Config {
 }
 
 impl Config {
-  fn new(args: &[String]) -> Config {
+  fn build(args: &[String]) -> Result<Config, &'static str> {
     if args.len() < 3 {
-      panic!("Not enough arguments");
+      return Err("not enough arguments");
     }
     let query = args[1].clone();
     let file_path = args[2].clone();
     
-    Config { query, file_path }
+    Ok(Config { query, file_path })
   }
 }
