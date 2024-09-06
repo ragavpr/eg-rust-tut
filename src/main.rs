e5ae19013459ea517cbe28beeb9e8baf17b5a639
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-06 21:51:41 +0530

    12.11 | logic seperated from main

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,14 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+    run(config);
+}
 
-    println!("With text:\n{contents}");
+fn run(config: Config) {
+  let contents = fs::read_to_string(config.file_path)
+      .expect("Should have been able to read the file");
+
+  println!("With text:\n{contents}");
 }
 
 struct Config {
