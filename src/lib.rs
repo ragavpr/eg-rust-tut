db79cee12c50f87452dcea8b2145bcc4a8565318
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-07 22:41:46 +0530

    20.19 | Job type alias for Box of trait

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ impl Worker {
     }
 }
 
-struct Job;
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
@@ -52,6 +52,8 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
-      
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
