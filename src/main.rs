a7f4b98cb9bb72a461fbbb2c84a3e41a294e3a57
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-07 13:04:56 +0530

    20.6 | Handling only the root RequestURI

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,21 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
     
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("www/hello.html").unwrap();
-    let length = contents.len();
+    //Only reading the first line of the request
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("www/hello.html").unwrap();
+        let length = contents.len();
+        
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
 
-    stream.write_all(response.as_bytes()).unwrap();
+        stream.write_all(response.as_bytes()).unwrap()      ;
+    } else {
+        // TODO: Handle Other RequestURI
+    }
 }
