59d70beeeabcaa936eac0f337029013a6c0f29ec
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-07 13:45:24 +0530

    20.10 | Simulating a slow request by sleeping for 5 seconds

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -20,13 +22,16 @@ fn handle_connection(mut stream: TcpStream) {
     //Only reading the first line of the request
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "www/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "www/404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
-
-    let contents = fs::read_to_string(filename).unwrap();
+    
+    let contents = fs::read_to_string(format!("www/{}", filename)).unwrap();
     let length = contents.len();
 
     let response =
