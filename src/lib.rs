05869a13c5aef7cfc77b7e73cbbae9449798f3f2
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-07 22:49:26 +0530

    20.21 | alt: thread receiver runner

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,12 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+        let thread = thread::spawn(move || {
+            while let Ok(job) = receiver.lock().unwrap().recv() {
+                println!("Worker {id} got a job; executing.");
 
-            println!("Worker {id} got a job; executing.");
-
-            job();
+                job();
+            }
         });
         Worker {id, thread}
     }
