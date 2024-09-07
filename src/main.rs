efb026a8e22d6eb3a12961da23e38b67b35e679d
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-07 13:12:32 +0530

    20.7 | Serve 404 page for other requests

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,14 @@ fn handle_connection(mut stream: TcpStream) {
 
         stream.write_all(response.as_bytes()).unwrap()      ;
     } else {
-        // TODO: Handle Other RequestURI
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("www/404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
